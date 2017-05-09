e7f7d3e3d2c74c86aad9f91e0e54b4e887659b4b
jitete <jitete93@gmail.com>
2017-05-09 15:22:03 +0800

    edit body to ned transform

--- drivers/px4flow/script/bodyToNED.py
+++ drivers/px4flow/script/bodyToNED.py
@@ -6,6 +6,7 @@ from geometry_msgs.msg import TwistStamped
 from std_msgs.msg import Header
 from numpy import *
 import tf
+import math
 
 q = array([0,0,0,1])
 v = array([0,0,0])
@@ -16,14 +17,15 @@ t_prev = 0
 z_now = 0
 t_now = 0
 
+yaw = 130.0/180 * math.pi  # from NED to Vicon frame
+
 def imucallback(msg):
-    q_enu = (msg.orientation.x, msg.orientation.y, msg.orientation.z, msg.orientation.w)
-    euler = tf.transformations.euler_from_quaternion(q_enu)
-    quaternion = tf.transformations.quaternion_from_euler(euler[0], -euler[1], -euler[2])
+    # attitude measured in ENU frame
     global q
-    q = array([quaternion[0], quaternion[1], quaternion[2], quaternion[3]])
+    q = array([msg.orientation.x, msg.orientation.y, msg.orientation.z, msg.orientation.w])
 
 def toNED(msg):
+    # fliter measured velocity
     global vx_arr, vy_arr
     vx_arr.pop(0)
     vx_arr.append(msg.velocity_x)
@@ -32,16 +34,18 @@ def toNED(msg):
 
     vx = 0.5*vx_arr[4]+0.2*vx_arr[3]+0.15*vx_arr[2]+0.1*vx_arr[1]+0.05*vx_arr[0]
     vy = 0.5*vy_arr[4]+0.2*vy_arr[3]+0.15*vy_arr[2]+0.1*vy_arr[1]+0.05*vy_arr[0]
-    v_body = array([vx, vy, 0]) * 1.2
-
+    v_body = array([vx, -vy, 0]) * 1.2
+    
+    # transform body velocity to ENU 
     global q
     [qx, qy, qz, qw] = [q[0], q[1], q[2], q[3]]
-    Tned = array([[1-2*qy*qy-2*qz*qz,   2*qx*qy-2*qz*qw,   2*qx*qz+2*qy*qw],
+    Tenu = array([[1-2*qy*qy-2*qz*qz,   2*qx*qy-2*qz*qw,   2*qx*qz+2*qy*qw],
                   [2*qx*qy + 2*qz*qw, 1-2*qx*qx-2*qz*qz,   2*qy*qz-2*qx*qw],
                   [2*qx*qz-2*qy*qw  , 2*qy*qz + 2*qx*qw, 1-2*qx*qx-2*qy*qy]])
 
-    v = dot(Tned, v_body)
+    v = dot(Tenu, v_body)
     
+    # estimate z velocity by height derivative
     global z_now, t_now, z_prev, t_prev
     z_now = msg.ground_distance
     t_now = msg.header.stamp.to_sec()
@@ -50,26 +54,45 @@ def toNED(msg):
     else:
         vz = 0
 
+    # ENU to NED: (x,y,z) -> (x,-y,-z)
     twist = TwistStamped()
     twist.header = Header()
     twist.header.frame_id = "ned"
-    twist.header.stamp = rospy.Time.now()
+    twist.header.stamp = msg.header.stamp
     twist.twist.linear.x = v[0]
-    twist.twist.linear.y = v[1]
+    twist.twist.linear.y = -v[1]
     twist.twist.linear.z = -vz
     pub.publish(twist)
 
     z_prev = z_now
     t_prev = t_now
 
+    # record data in vicon frame, compare with vicon
+    q_ned_vicon = tf.transformations.quaternion_from_euler(math.pi, 0, -yaw)
+    [qx, qy, qz, qw] = [q_ned_vicon[0], q_ned_vicon[1], q_ned_vicon[2], q_ned_vicon[3]]
+    Tv = array([[1-2*qy*qy-2*qz*qz,   2*qx*qy-2*qz*qw,   2*qx*qz+2*qy*qw],
+                  [2*qx*qy + 2*qz*qw, 1-2*qx*qx-2*qz*qz,   2*qy*qz-2*qx*qw],
+                  [2*qx*qz-2*qy*qw  , 2*qy*qz + 2*qx*qw, 1-2*qx*qx-2*qy*qy]])
+    vr = dot(Tv, array([v[0],-v[1],0]))
+
+    outtxt.write(str.format("{0:.9f} ", msg.header.stamp.to_sec()))
+    outtxt.write(str.format("{0:.9f} ", vr[0]))
+    outtxt.write(str.format("{0:.9f} ", vr[1]))
+    outtxt.write(str.format("{0:.9f} ", vz))
+    outtxt.write('0 ')
+    outtxt.write('0 ')
+    outtxt.write('0 ')
+    outtxt.write('0\n')
+
+
 if __name__ == '__main__':
+
+    outtxt = open('/home/jitete/drones/src/px-ros-pkg/drivers/px4flow/script/flow5.txt','w')
+    outtxt.write('# text file' + '\n# format: time stamp x y z qx qy qz qw\n')
+
     rospy.init_node('bodyToNED')
     rospy.Subscriber('/imu/data', Imu, imucallback)
     pub = rospy.Publisher('velocity', TwistStamped, queue_size=0)	
-#    rate = rospy.Rate(1)
-#    for i in xrange(5):
-#        rate.sleep()
-
     rospy.Subscriber('/px4flow/opt_flow', OpticalFlow, toNED)
     rospy.spin()
 
